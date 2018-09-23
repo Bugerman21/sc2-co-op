76e04deb553f898b171601cd7cb70678f9480c61
Victor <bugerman2011@gmail.com>
2018-09-23 14:53:43 +0300

    Start test
    Himer Rokovoy
    Muhaha ha

---
+++ bootstrap/ReadMe.txt
@@ -0,0 +1,17 @@
+Подключение:
+===================================================================================
+После <title>
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+перед закрытием </body>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+===================================================================================
+
+
+container - это основной элемент bootstrap 4
+Есть два типа:
+contaner - имеется макс ширина
+container-fluid - ширина всегда 100%
---
+++ js/script.js
@@ -0,0 +1,49 @@
+var heroes = ["Raynor", "Kerrigan", "Artanis", "Svon",
+			 "Zagara", "Vorazun", "Karaks", "Abatur",
+			 "Alarak", "Nova", "Stukov", "Phoenix",
+			 "Dehaka", "Han & Horner", "Tychus"];
+
+var temp = 0;
+var count = 0;
+
+
+document.write("<table class='mx-auto mt-4'>");
+
+for (var i = 0; i < 4; i++) {
+
+  document.write("<tr>");
+
+  for (var j = 0; j < 4; j++) {
+	count++;
+      document.write("<td class='px-4'>" + "<a href='#'><img src='img/" + count + ".png' class='part_two_images'></a>" + "</td>");
+	 if (count == heroes.length) {
+		    break;
+	  }
+  }
+
+  document.write("</tr>");
+
+  /*---------------------------------------------------------------------------------------------------*/
+   document.write("<tr>");
+
+  for (var j = 0; j < 4; j++) {
+	  if (heroes[j+temp] == undefined) {
+		    break;
+	  }
+      document.write("<td class='color pb-5'>" + heroes[j+temp] + "</td>");
+  }
+
+temp += 4;
+  document.write("</tr>");
+
+}
+
+document.write("</table><br><br>");
+
+
+
+
+
+/* if (count == heroes.length) {
+		break;
+	  }*/
